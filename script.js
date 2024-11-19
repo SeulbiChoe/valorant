6d675b6717b5ca656f5f6b0192b60180b3dfb247
SeulbiChoe <hiitzsb@naver.com>
2024-11-19 21:30:54 +0900

    commit_1119

--- script.js
+++ script.js
@@ -47,8 +47,8 @@ setTimeout(()=>{
 window.addEventListener("scroll", function(){
     let scrollTop = window.scrollY;
     let windowHeight = window.innerHeight;
-    let introTxtDiv = document.querySelector(".contents .introduction .txt1");
-    let introTxt = document.querySelector(".contents .introduction .txt1 .mo_txt");
+    let introTxtDiv = document.querySelector(".contents .valorant .txt1");
+    let introTxt = document.querySelector(".contents .valorant .txt1 .mo_txt");
     let introTxtHeight = introTxtDiv.offsetHeight;
     // offsetHeight 에 자꾸 ()이거 함수 넣어서 오류 나왔음 ㅋㅋ...
     //맨 위부터 텍스트2 까지의 와이축 거리임 스크롤탑으로 찍어보면 알 수 있다.
@@ -72,8 +72,8 @@ window.addEventListener("scroll", function(){
 window.addEventListener("scroll", function(){
     let scrollTop = window.scrollY;
     let windowHeight = window.innerHeight;
-    let introTxtDiv = document.querySelector(".contents .introduction .txt1");
-    let introTxt = document.querySelector(".contents .introduction .txt1 .dt_txt");
+    let introTxtDiv = document.querySelector(".contents .valorant .txt1");
+    let introTxt = document.querySelector(".contents .valorant .txt1 .dt_txt");
     let introTxtHeight = introTxtDiv.offsetHeight;
     let distanceFromHtml = introTxtDiv.getBoundingClientRect().top + this.window.scrollY;
 
@@ -91,7 +91,7 @@ window.addEventListener("scroll", function(){
     let scrollTop = window.scrollY;
     let windowHeight = window.innerHeight;
     
-    let newsImg = document.querySelector(".contents .introduction .news .news_img");
+    let newsImg = document.querySelector(".contents .valorant .news .news_img");
     let newImgHeight = newsImg.offsetHeight;
     let imgFromHtml = newsImg.getBoundingClientRect().top + this.window.scrollY;
     if (imgFromHtml + newImgHeight < scrollTop + windowHeight) {
@@ -101,20 +101,52 @@ window.addEventListener("scroll", function(){
         newsImg.classList.remove("active")
     }
 
-    let newsDate = document.querySelector(".contents .introduction .news .news_date");
+    let newsDate = document.querySelector(".contents .valorant .news .news_date");
     let newsDateHeight = newsDate.offsetHeight;
     let dateFromHtml = newsDate.getBoundingClientRect().top + this.window.scrollY;
 
-    let newsTitle = document.querySelector(".contents .introduction .news .news_title");
+    let newsTitle = document.querySelector(".contents .valorant .news .news_title");
     let newsTitleHeight = newsTitle.offsetHeight;
     let tileFromHtml = newsTitle.getBoundingClientRect().top + this.window.scrollY;
 
-    let newsDecs = document.querySelector(".contents .introduction .news .news_decs");
+    let newsDecs = document.querySelector(".contents .valorant .news .news_decs");
     let newsDecsHeight = newsDecs.offsetHeight;
     let decsFromHtml = newsDecs.getBoundingClientRect().top + this.window.scrollY;
 
-    let newsPagenation = document.querySelector(".contents .introduction .news .pagenation");
+    let newsPagenation = document.querySelector(".contents .valorant .news .pagenation");
     let newsPagenationHeight = newsPagenation.offsetHeight;
     let pagenationFromHtml = newsPagenation.getBoundingClientRect().top + this.window.scrollY;
 
-})
+});
+
+
+//뉴스 슬라이드 캐러셀
+let newsContents = document.querySelector(".contents .valorant .news .news_contents");
+let news = document.querySelectorAll(".contents .valorant .news .news_contents .item");
+let next = document.querySelector(".contents .valorant .news .pagenation .control .next");
+let prev = document.querySelector(".contents .valorant .news .pagenation .control .prev");
+let now = 0;
+let last = news.length;
+
+next.addEventListener("click", function(){
+    // for문 하면 안되고 이프문으로 하고 클릭하면 +1 하는 식으로 해야할 듯 함 어차피 통채로 움직이니까
+
+    if (now == 0 || now < last - 1) {
+        newsContents.style.transform = `translateX(-${now+1}00%)`;
+        now += 1;
+        console.log(now)
+    }
+});
+
+prev.addEventListener("click", function(e){
+
+    if (now > 1) {
+        newsContents.style.transform = `translateX(-${now-1}00%)`;
+        console.log(now)
+        now -= 1;
+    } else if (now == 1) {
+        newsContents.style.transform = `translateX(0%)`;
+        e.preventDefault();
+        now = 0;
+    }
+});
